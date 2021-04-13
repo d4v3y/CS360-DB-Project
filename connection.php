360ac3ae33f4380412c4363835c0c5a06aa503e1
d4v3y <45963494+d4v3y@users.noreply.github.com>
2021-04-13 01:06:28 -0700

    Updated connection file again

--- connection.php
+++ connection.php
@@ -4,18 +4,18 @@ $dbuser = "root";
 $dbpass = "";
 $dbname = "cs360project";
 
-$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-if ($con->connect_error) {
+if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
     die("Failed to connect!");
 }
 
 /*** WIP: This almost connects to the shared server. ***/
 // $dbhost = "localhost";
 // $dbuser = "httpdh33";
-// $dbname = "db2";
+// $dbname = "db1";
 // $dbport = "3339";
 // $dbsocket = "/vols/sdb37/httpdh33_db/httpdh33_db.sock";
+
+// $conn = new mysqli($dbhost, $dbuser, "", $dbname, $dbport, $dbsocket);
 
 // if (!$con = mysqli_connect($dbhost, $dbuser, "", $dbname, $dbport, $dbsocket)) {
 //     die("Failed to connect!");
